c9632ffdbb33b4841059640888502bce23a2b478
Rennie130 <renenaude503@gmail.com>
2026-04-09 13:46:35 +0800

    Added enemy patrol
    Added enemy patrol code to the EnemyController script.
    
    Added patrol points for the enemy to move between.

--- Assets/Scripts/Movement/EnemyController.cs
+++ Assets/Scripts/Movement/EnemyController.cs
@@ -9,30 +9,46 @@ using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
+    [Header ("References")]
+    // sets the target we are chasing
     public Transform target;
+    // a Transform array to store patrol point locations
+    public Transform[] patrolPoints;
+
+
+    [Header ("Settings")]
+    // Distance enemy stops from mech to attack
     public float attackDistance;
+    // time an enemy stops at each patrol point
+    public float patrolWaitTime = 2f;
+    // used in "Patrol" method to check the distance between player and next patrol point
+    public float stopAtDistance = 0.5f;
 
     private NavMeshAgent agent;
+    // distance between enemy and mech
     private float distance;
+    private int currentPatrolIndex;
+    private bool isWaiting;
+
+
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        // send enemy to first patrol point
+        GoToNext();
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        /*
-        if (target != null)
-        {
-         
-        }
-        else
-        {
-            UnityEngine.Debug.Log("Mech destroyed and no longer chaseable.");
-        } */
+        Patrol();
+        // if (target == null) return;
         
+
+        /*
         // checks and assigns the distance between enemy and mech
         distance = UnityEngine.Vector3.Distance(agent.transform.position, target.position);
         // if distance is less than specified attack distance, enemy stops.
@@ -46,6 +62,56 @@ public class EnemyController : MonoBehaviour
             agent.isStopped = false;
             //updates the enemy destination to player mech's location
             agent.destination = target.position;
-        }
+        } 
+        */
     }
+
+
+    // method to patrol between points
+    private void Patrol()
+    {
+        if (isWaiting) return;
+
+        if (!agent.pathPending && agent.remainingDistance <= stopAtDistance)
+        {
+            UnityEngine.Debug.Log("Initiate Coroutine");
+            //start the coroutine
+            StartCoroutine(WaitAtPatrolPoint());
+        }
+
+    }
+
+
+    // A coroutine method that can suspend execution and resume at a later time
+    private IEnumerator WaitAtPatrolPoint()
+    {
+        isWaiting = true;
+        agent.isStopped = true; 
+
+        // stop/wait for the amount of time set in the patrolWaitTime variable
+        // Suspends the coroutine execution for the specified scaled time in seconds
+        yield return new WaitForSecondsRealtime(patrolWaitTime);
+
+        // after set time is up, continue moving to next point.
+        UnityEngine.Debug.Log("Done waiting");
+        agent.isStopped = false;
+        GoToNext();
+        isWaiting = false;
+    }
+
+
+    // method that allows us to go to next patrol point
+    private void GoToNext()
+    {
+        if (patrolPoints.Length == 0) return;
+
+        agent.SetDestination(patrolPoints[currentPatrolIndex].position);
+        UnityEngine.Debug.Log("On way to patrol point");
+
+        // loop to next item in array (increasing currentPatrolIndex by 1) or; 
+        // change it back to 0 if no more points to shift through.Ensures resulting value is always between 0 and array.Length
+        currentPatrolIndex = (currentPatrolIndex + 1) % patrolPoints.Length;
+        UnityEngine.Debug.Log("Patrol Point updated");
+    }
+
 }
