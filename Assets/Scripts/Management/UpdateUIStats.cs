efb9cc178ccca662f8a11189d08bf6fcfd5cb518
Rennie130 <renenaude503@gmail.com>
2026-06-06 20:56:56 +0800

    Updated UI to include HealthBars
    - Added health bar sliders to UI
    - Updated sliders through linking to UpdateUI script.

--- Assets/Scripts/Management/UpdateUIStats.cs
+++ Assets/Scripts/Management/UpdateUIStats.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class UpdateUIStats : MonoBehaviour
@@ -11,31 +12,54 @@ public class UpdateUIStats : MonoBehaviour
     public TMP_Text mechHealthText;
     public TMP_Text districtHealthText;
 
+    public Slider playerHealthSlider;
+    public Slider mechHealthSlider;
+    public Slider signalStrengthSlider;
+    public Slider districtHealthSlider;
+    public Slider enemyHealthSlider;
+
+    void Start()
+    {
+        //Max Values
+        playerHealthSlider.maxValue = GameManager.Instance.playerHealth.maxHealth;
+        mechHealthSlider.maxValue = GameManager.Instance.mechHealth.maxHealth;
+
+        //Current Values
+        playerHealthSlider.value = GameManager.Instance.playerHealth.currentHealth;
+        mechHealthSlider.value = GameManager.Instance.mechHealth.currentHealth;
+        signalStrengthSlider.value = GameManager.Instance.signalStrength.signalStrength;
+        districtHealthSlider.value = GameManager.Instance.districtHealth.currentDistrictHealth;
+    }
+    
     // Update is called once per frame
     void Update()
     {
         if(GameManager.Instance.playerHealth)
         {
             //Update Player Health display in UI
-            playerHealthText.text = "Player Health " + GameManager.Instance.playerHealth.currentHealth.ToString() + "/10";
+            playerHealthSlider.value = GameManager.Instance.playerHealth.currentHealth;
+            //playerHealthText.text = "Player Health " + GameManager.Instance.playerHealth.currentHealth.ToString() + "/10";
         }        
 
         if(GameManager.Instance.mechHealth)
         {
             //Update Mech Health display in UI
-            mechHealthText.text = "Mech Health " + GameManager.Instance.mechHealth.currentHealth.ToString() + "/20";
+            mechHealthSlider.value = GameManager.Instance.mechHealth.currentHealth;
+            //mechHealthText.text = "Mech Health " + GameManager.Instance.mechHealth.currentHealth.ToString() + "/20";
         }
 
         if(GameManager.Instance.signalStrength)
         {
             //Update Signal Strength display in UI
-            signalStrengthText.text = "Signal Strength " + GameManager.Instance.signalStrength.signalStrength.ToString("P0");
+            signalStrengthSlider.value = GameManager.Instance.signalStrength.signalStrength;
+            //signalStrengthText.text = "Signal Strength " + GameManager.Instance.signalStrength.signalStrength.ToString("P0");
         }
 
         if(GameManager.Instance.districtHealth)
         {
-            //Update Mech Health display in UI
-            districtHealthText.text = "District Health " + GameManager.Instance.districtHealth.currentDistrictHealth.ToString() + "/160";
+            //Update District Health display in UI
+            districtHealthSlider.value = GameManager.Instance.districtHealth.currentDistrictHealth;
+            //districtHealthText.text = "District Health " + GameManager.Instance.districtHealth.currentDistrictHealth.ToString() + "/160";
         }
 
     }
