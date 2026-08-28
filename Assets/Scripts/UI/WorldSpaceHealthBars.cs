4e5725f47841f1ec8e1d62336aeb5701b4bb96bf
RennieDev <renenaude503@gmail.com>
2026-08-28 12:05:38 +0800

    Cont. updates on Enemy health bar script
    Just adjusted what the variables are called in the script

--- Assets/Scripts/UI/WorldSpaceHealthBars.cs
+++ Assets/Scripts/UI/WorldSpaceHealthBars.cs
@@ -5,29 +5,39 @@ using UnityEngine.UI;
 using TMPro;
 public class WorldSpaceHealthBars : MonoBehaviour
 {
-    public Health enemyHealth;
-    public TMP_Text enemyHealthText;
-    public Slider enemyHealthSlider;
+    [Header("Enemy References")]
+    public Health objectHealth;
+    
+
+    [Header("Destructible Object References")]
+    public Building destructibleObjectHealth;
+
+    [Header("Shared References")]
+    public TMP_Text objectHealthText;
+    public Slider objectHealthSlider;
     public Transform canvasTransform;
 
     // Start is called before the first frame update
     void Start()
     {
-        // Max Value
-        enemyHealthSlider.maxValue = enemyHealth.maxHealth;
+        // Enemy Max Value
+        objectHealthSlider.maxValue = objectHealth.maxHealth;
 
-        // Current Value
-        enemyHealthSlider.value = enemyHealth.currentHealth;
+        // Enemy Current Value
+        objectHealthSlider.value = objectHealth.currentHealth;
+
     }    
 
     // Update is called once per frame
     void Update()
     {
-        enemyHealthSlider.value = enemyHealth.currentHealth;
-        enemyHealthText.text = "Enemy Health " + enemyHealth.currentHealth.ToString() + "/10";
+        
+        objectHealthSlider.value = objectHealth.currentHealth;
+        objectHealthText.text = gameObject.tag + " Health " + objectHealth.currentHealth.ToString() + "/10"; 
+        
     }
 
-    // Late update runs after all other updates to ensure canvas moves with the enemy's current position
+    // Late update runs after all other updates to ensure canvas moves with the object's current position
     private void LateUpdate()
     {
         //tells the canvas to look at the main camera's transform.
